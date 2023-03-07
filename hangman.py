83d347a356f4ca00305442841463e95b9f03802c
SammyIsCammy <44585438+SammyIsCammy@users.noreply.github.com>
2023-03-07 16:18:02 -0500

    Added more ASCII art and made functions.

--- hangman.py
+++ hangman.py
@@ -1,15 +1,21 @@
 import random
-
-#list_of_words = ["REPRESENT", "EXHAUST", "THRUST", "BLAME", "INVEST", "REINFORCE", "VIEW",
-#"TRANSFORM", "EXECUTE", "RAIN"]
+import os
 
 list_of_words = ["rain"]
 
 # Selects a random word from the list of words and separates the characters to form a list. EXAMPLE: ["R", "A", "I", "N"]
 selected_word = list(random.choice(list_of_words))
-print(selected_word)
 
-print("""
+
+# Number of underscrolls to appear that will disappear upon every correct character guess
+blanks = [] 
+for blank in range(len(selected_word)):
+    blanks.append("_")
+
+
+# Function that prints the HANGMAN banner and prints the amount of letters to fill in
+def banner():
+    print("""
  _                                             
 | |                                            
 | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
@@ -20,14 +26,51 @@ print("""
                    |___/                       
                    """)
 
-# Number of underscrolls to appear that will disappear upon every correct character guess
-blanks = [] 
-for blank in range(len(selected_word)):
-    blanks.append("_")
-print(*blanks)
+    print(*blanks)
 
 
-while blanks != selected_word:
+# Function that runs if you lose
+def lose():
+    os.system("cls")
+    print("YOU HUNG YOUR MAN!")
+    print("""
+                _______
+                |/      |
+                |      (_)
+                |      \|/
+                |       |
+                |      / \\
+                |
+            ____|___""")
+    
+
+# Function that runs if you win
+def win():
+    os.system("cls")
+    print("""             
+            OOOOOOOOOOO
+         OOOOOOOOOOOOOOOOOOO
+      OOOOOO  OOOOOOOOO  OOOOOO
+    OOOOOO      OOOOO      OOOOOO
+  OOOOOOOO  #   OOOOO  #   OOOOOOOO
+ OOOOOOOOOO    OOOOOOO    OOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO
+ OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO
+  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO
+    OOOOO   OOOOOOOOOOOOOOO   OOOO
+      OOOOOO   OOOOOOOOO   OOOOOO
+         OOOOOO         OOOOOO
+             OOOOOOOOOOOO\n""")
+    print("CONGRATS!!! YOU WIN!!!")
+
+
+counter = len(selected_word)
+
+
+while blanks != selected_word and counter > 0:
+    banner()
     guess = input("Guess a letter: ")
     if guess.isalpha() == False:
         print("You cannot enter a number, symbol, or space!")
@@ -37,6 +80,16 @@ while blanks != selected_word:
         if guess == selected_word[i]:
             blanks[i] = guess
 
+
+    if guess in selected_word or guess.isalpha() == False:
+        counter = counter
+    elif not guess in selected_word:
+        counter -= 1
+
     print(*blanks)
 
+    if counter == 0:
+        lose()
 
+    if blanks == selected_word:
+        win()
